932d2390b3c87e56dcbc10fef581b55bc090f804
Tobi <arotimi.tobiloba@gmail.com>
2022-03-28 14:44:00 +0100

    added more test

--- src/components/Navigation/SignUp/SignUp.test.js
+++ src/components/Navigation/SignUp/SignUp.test.js
@@ -9,6 +9,8 @@ test('if user passes validations, create account button is active', async () => 
 
     userEvent.type(screen.getByPlaceholderText(/Enter Firstname/i), "firstName");
     userEvent.type(screen.getByPlaceholderText(/Enter LastName/i), "LastName");
+    userEvent.type(screen.getByPlaceholderText(/Enter Email/i), "email");
+    userEvent.type(screen.getByPlaceholderText(/Enter Password/i), "password");
 
     expect(await screen.findByRole('button', { name: /create account/i })).toBeEnabled();
 });
